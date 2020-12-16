3d2ce01f3765f2468c1d304849757cea954eb21f
chenshaowei57 <845643821@qq.com>
2020-12-16 18:58:52 +0800

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,158 @@
+# coding: UTF-8
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+
+
+class OriginalDataset(Dataset):
+    def __init__(self, pre_data):
+        self._forward_asp_query = pre_data['_forward_asp_query']
+        self._forward_opi_query = pre_data['_forward_opi_query']
+        self._forward_asp_answer_start = pre_data['_forward_asp_answer_start']
+        self._forward_asp_answer_end = pre_data['_forward_asp_answer_end']
+        self._forward_opi_answer_start = pre_data['_forward_opi_answer_start']
+        self._forward_opi_answer_end = pre_data['_forward_opi_answer_end']
+        self._forward_asp_query_mask = pre_data['_forward_asp_query_mask']
+        self._forward_opi_query_mask = pre_data['_forward_opi_query_mask']
+        self._forward_asp_query_seg = pre_data['_forward_asp_query_seg']
+        self._forward_opi_query_seg = pre_data['_forward_opi_query_seg']
+
+        self._backward_asp_query = pre_data['_backward_asp_query']
+        self._backward_opi_query = pre_data['_backward_opi_query']
+        self._backward_asp_answer_start = pre_data['_backward_asp_answer_start']
+        self._backward_asp_answer_end = pre_data['_backward_asp_answer_end']
+        self._backward_opi_answer_start = pre_data['_backward_opi_answer_start']
+        self._backward_opi_answer_end = pre_data['_backward_opi_answer_end']
+        self._backward_asp_query_mask = pre_data['_backward_asp_query_mask']
+        self._backward_opi_query_mask = pre_data['_backward_opi_query_mask']
+        self._backward_asp_query_seg = pre_data['_backward_asp_query_seg']
+        self._backward_opi_query_seg = pre_data['_backward_opi_query_seg']
+
+        self._sentiment_query = pre_data['_sentiment_query']
+        self._sentiment_answer = pre_data['_sentiment_answer']
+        self._sentiment_query_mask = pre_data['_sentiment_query_mask']
+        self._sentiment_query_seg = pre_data['_sentiment_query_seg']
+
+        self._aspect_num = pre_data['_aspect_num']
+        self._opinion_num = pre_data['_opinion_num']
+
+
+class ReviewDataset(Dataset):
+    def __init__(self, train, dev, test, set):
+        '''
+        评论数据集
+        :param train: list, training set of 14 lap, 14 res, 15 res, 16 res
+        :param dev: list, the same
+        :param test: list, the same
+        '''
+        self._train_set = train
+        self._dev_set = dev
+        self._test_set = test
+        if set == 'train':
+            self._dataset = self._train_set
+        elif set == 'dev':
+            self._dataset = self._dev_set
+        elif set == 'test':
+            self._dataset = self._test_set
+
+        self._forward_asp_query = self._dataset._forward_asp_query
+        self._forward_opi_query = self._dataset._forward_opi_query
+        self._forward_asp_answer_start = self._dataset._forward_asp_answer_start
+        self._forward_asp_answer_end = self._dataset._forward_asp_answer_end
+        self._forward_opi_answer_start = self._dataset._forward_opi_answer_start
+        self._forward_opi_answer_end = self._dataset._forward_opi_answer_end
+        self._forward_asp_query_mask = self._dataset._forward_asp_query_mask
+        self._forward_opi_query_mask = self._dataset._forward_opi_query_mask
+        self._forward_asp_query_seg = self._dataset._forward_asp_query_seg
+        self._forward_opi_query_seg = self._dataset._forward_opi_query_seg
+        self._backward_asp_query = self._dataset._backward_asp_query
+        self._backward_opi_query = self._dataset._backward_opi_query
+        self._backward_asp_answer_start = self._dataset._backward_asp_answer_start
+        self._backward_asp_answer_end = self._dataset._backward_asp_answer_end
+        self._backward_opi_answer_start = self._dataset._backward_opi_answer_start
+        self._backward_opi_answer_end = self._dataset._backward_opi_answer_end
+        self._backward_asp_query_mask = self._dataset._backward_asp_query_mask
+        self._backward_opi_query_mask = self._dataset._backward_opi_query_mask
+        self._backward_asp_query_seg = self._dataset._backward_asp_query_seg
+        self._backward_opi_query_seg = self._dataset._backward_opi_query_seg
+        self._sentiment_query = self._dataset._sentiment_query
+        self._sentiment_answer = self._dataset._sentiment_answer
+        self._sentiment_query_mask = self._dataset._sentiment_query_mask
+        self._sentiment_query_seg = self._dataset._sentiment_query_seg
+        self._aspect_num = self._dataset._aspect_num
+        self._opinion_num = self._dataset._opinion_num
+
+    def get_batch_num(self, batch_size):
+        return len(self._forward_asp_query) // batch_size
+
+    def __len__(self):
+        return len(self._forward_asp_query)
+
+    def __getitem__(self, item):
+        forward_asp_query = self._forward_asp_query[item]
+        forward_opi_query = self._forward_opi_query[item]
+        forward_asp_answer_start = self._forward_asp_answer_start[item]
+        forward_asp_answer_end = self._forward_asp_answer_end[item]
+        forward_opi_answer_start = self._forward_opi_answer_start[item]
+        forward_opi_answer_end = self._forward_opi_answer_end[item]
+        forward_asp_query_mask = self._forward_asp_query_mask[item]
+        forward_opi_query_mask = self._forward_opi_query_mask[item]
+        forward_asp_query_seg = self._forward_asp_query_seg[item]
+        forward_opi_query_seg = self._forward_opi_query_seg[item]
+        backward_asp_query = self._backward_asp_query[item]
+        backward_opi_query = self._backward_opi_query[item]
+        backward_asp_answer_start = self._backward_asp_answer_start[item]
+        backward_asp_answer_end = self._backward_asp_answer_end[item]
+        backward_opi_answer_start = self._backward_opi_answer_start[item]
+        backward_opi_answer_end = self._backward_opi_answer_end[item]
+        backward_asp_query_mask = self._backward_asp_query_mask[item]
+        backward_opi_query_mask = self._backward_opi_query_mask[item]
+        backward_asp_query_seg = self._backward_asp_query_seg[item]
+        backward_opi_query_seg = self._backward_opi_query_seg[item]
+        sentiment_query = self._sentiment_query[item]
+        sentiment_answer = self._sentiment_answer[item]
+        sentiment_query_mask = self._sentiment_query_mask[item]
+        sentiment_query_seg = self._sentiment_query_seg[item]
+        aspect_num = self._aspect_num[item]
+        opinion_num = self._opinion_num[item]
+
+        return {"forward_asp_query": np.array(forward_asp_query),
+                "forward_opi_query": np.array(forward_opi_query),
+                "forward_asp_answer_start": np.array(forward_asp_answer_start),
+                "forward_asp_answer_end": np.array(forward_asp_answer_end),
+                "forward_opi_answer_start": np.array(forward_opi_answer_start),
+                "forward_opi_answer_end": np.array(forward_opi_answer_end),
+                "forward_asp_query_mask": np.array(forward_asp_query_mask),
+                "forward_opi_query_mask": np.array(forward_opi_query_mask),
+                "forward_asp_query_seg": np.array(forward_asp_query_seg),
+                "forward_opi_query_seg": np.array(forward_opi_query_seg),
+                "backward_asp_query": np.array(backward_asp_query),
+                "backward_opi_query": np.array(backward_opi_query),
+                "backward_asp_answer_start": np.array(backward_asp_answer_start),
+                "backward_asp_answer_end": np.array(backward_asp_answer_end),
+                "backward_opi_answer_start": np.array(backward_opi_answer_start),
+                "backward_opi_answer_end": np.array(backward_opi_answer_end),
+                "backward_asp_query_mask": np.array(backward_asp_query_mask),
+                "backward_opi_query_mask": np.array(backward_opi_query_mask),
+                "backward_asp_query_seg": np.array(backward_asp_query_seg),
+                "backward_opi_query_seg": np.array(backward_opi_query_seg),
+                "sentiment_query": np.array(sentiment_query),
+                "sentiment_answer": np.array(sentiment_answer),
+                "sentiment_query_mask": np.array(sentiment_query_mask),
+                "sentiment_query_seg": np.array(sentiment_query_seg),
+                "aspect_num": np.array(aspect_num),
+                "opinion_num": np.array(opinion_num)
+                }
+
+
+def generate_fi_batches(dataset, batch_size, shuffle=True, drop_last=True, ifgpu=True):
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size,
+                            shuffle=shuffle, drop_last=drop_last)
+
+    for data_dict in dataloader:
+        out_dict = {}
+        for name, tensor in data_dict.items():
+            if ifgpu:
+                out_dict[name] = data_dict[name].cuda()
+            else:
+                out_dict[name] = data_dict[name]
+        yield out_dict
---
+++ utils.py
@@ -0,0 +1,90 @@
+# coding: UTF-8
+import torch
+from torch.nn import functional as F
+import logging
+
+
+def normalize_size(tensor):
+    if len(tensor.size()) == 3:
+        tensor = tensor.contiguous().view(-1, tensor.size(2))
+    elif len(tensor.size()) == 2:
+        tensor = tensor.contiguous().view(-1)
+
+    return tensor
+
+
+def calculate_entity_loss(pred_start, pred_end, gold_start, gold_end):
+    pred_start = normalize_size(pred_start)
+    pred_end = normalize_size(pred_end)
+    gold_start = normalize_size(gold_start)
+    gold_end = normalize_size(gold_end)
+    
+    weight = torch.tensor([1, 3]).float().cuda()
+
+    loss_start = F.cross_entropy(pred_start, gold_start.long(), size_average=False, weight=weight, ignore_index=-1)
+    loss_end = F.cross_entropy(pred_end, gold_end.long(), size_average=False, weight=weight, ignore_index=-1)
+
+    return 0.5 * loss_start + 0.5 * loss_end
+
+
+def calculate_sentiment_loss(pred_sentiment, gold_sentiment):
+    return F.cross_entropy(pred_sentiment, gold_sentiment.long(), size_average=False, ignore_index=-1)
+
+
+def get_logger(filename, verbosity=1, name=None):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(filename, "w")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
+
+def filter_prob(f_asp_prob, f_opi_prob, f_opi_start_index, f_opi_end_index, beta):
+    filter_start = []
+    filter_end = []
+    for idx in range(len(f_opi_prob)):
+        if f_asp_prob*f_opi_prob[idx]>=beta:
+            filter_start.append(f_opi_start_index[idx])
+            filter_end.append(f_opi_end_index[idx])
+    return filter_start, filter_end
+
+def filter_unpaired(start_prob, end_prob, start, end):
+    filtered_start = []
+    filtered_end = []
+    filtered_prob = []
+    if len(start)>0 and len(end)>0:
+        length = start[-1]+1 if start[-1]>=end[-1] else end[-1]+1
+        temp_seq = [0]*length
+        for s in start:
+            temp_seq[s]+=1
+        for e in end:
+            temp_seq[e]+=2
+        last_start = -1
+        for idx in range(len(temp_seq)):
+            assert temp_seq[idx]<4
+            if temp_seq[idx] == 1:
+                last_start = idx
+            elif temp_seq[idx] == 2:
+                if last_start!=-1 and idx-last_start<5:
+                    filtered_start.append(last_start)
+                    filtered_end.append(idx)
+                    prob = start_prob[start.index(last_start)] * end_prob[end.index(idx)]
+                    filtered_prob.append(prob)
+                last_start = -1
+            elif temp_seq[idx] == 3:
+                filtered_start.append(idx)
+                filtered_end.append(idx)
+                prob = start_prob[start.index(idx)] * end_prob[end.index(idx)]
+                filtered_prob.append(prob)
+                last_start = -1
+    return filtered_start, filtered_end, filtered_prob
